0a6a6e255209eec2e250d33ca65f717521fe039c
perlancar (@pc-office) <perlancar@gmail.com>
2015-10-01 18:49:31 +0700

    Update

--- lib/Bencher/Scenario/LevenshteinModules.pm
+++ lib/Bencher/Scenario/LevenshteinModules.pm
@@ -8,25 +8,21 @@ use strict;
 use warnings;
 
 our $scenario = {
-    items => [
+    participants => [
         {
-            type     => 'function_call',
-            template => "PERLANCAR::Text::Levenshtein::editdist(<word1>, <word2>)"
+            fcall_template => "PERLANCAR::Text::Levenshtein::editdist(<word1>, <word2>)",
         },
         {
-            type     => 'function_call',
-            template => "Text::Levenshtein::fastdistance(<word1>, <word2>)",
+            fcall_template => "Text::Levenshtein::fastdistance(<word1>, <word2>)",
         },
         {
-            type     => 'function_call',
-            template => "Text::Levenshtein::XS::distance(<word1>, <word2>)",
+            fcall_template => "Text::Levenshtein::XS::distance(<word1>, <word2>)",
         },
         {
-            type     => 'function_call',
-            template => "Text::LevenshteinXS::distance(<word1>, <word2>)",
+            fcall_template => "Text::LevenshteinXS::distance(<word1>, <word2>)",
         },
     ],
-    function_args_sets => [
+    data_sets => [
         { args => {word1=>"a"      , word2=>"aa"},      result => 1 },
         { args => {word1=>"foo"    , word2=>"bar"},     result => 3 },
         { args => {word1=>"program", word2=>"porgram"}, result => 2 },
